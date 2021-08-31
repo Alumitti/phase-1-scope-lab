bed2d8854a367228efa21304ac0f4366d03fc71d
Connor <alumitti@DESKTOP-13091IV.localdomain>
2021-08-31 15:17:02 -0700

    commit tree 8e4493486238ca27af985cb2fa17c4c02b2b1062

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
-// Write your solution in this file!
+var customerName = 'bob';
+const leastFavoriteCustomer = 'bobie';
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase();
+}
+
+function setBestCustomer() {
+    bestCustomer = 'not bob';
+}
+
+function overwriteBestCustomer() {
+    window.bestCustomer = 'maybe bob';
+}
+
+function changeLeastFavoriteCustomer() {
+    leastFavoriteCustomer = 'dad';
+}
